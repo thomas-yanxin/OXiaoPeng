a82f14c5dc62e44ae516364550f63c2a22b3999e
thomas-yanxin <yx20001210@163.com>
2023-02-07 16:36:15 +0800

    update chatgpt

--- main-chatgpt.py
+++ main-chatgpt.py
@@ -1,21 +1,12 @@
-import random
 import sys
 import time
 
 import ntchat
-from revChatGPT.revChatGPT import Chatbot
+from revChatGPT.Official import Chatbot
 
-from chatgpt.config import getToken
-
-config = getToken()
-
-user_session = dict()
-# 初始化bot
-chatbot = Chatbot(config)
 wechat = ntchat.WeChat()
 # 打开pc微信, smart: 是否管理已经登录的微信
 wechat.open(smart=True)
-
 
 # 等待登录
 wechat.wait_login()
@@ -24,34 +15,9 @@ rooms = wechat.get_rooms()
 
 myself_wxid = wechat.get_self_info()['wxid']
 
+openai_key = '******************'
 
-def get_chat_response(session_id, prompt):
-    # 刷新seesion_token
-    chatbot.refresh_session()
-
-    if session_id in user_session:
-        # 如果在三分钟内再次发起对话则使用相同的会话ID
-        if time.time() < user_session[session_id]['timestamp'] + 60 * 3:
-            chatbot.conversation_id = user_session[session_id]['conversation_id']
-            chatbot.parent_id = user_session[session_id]['parent_id']
-        else:
-            chatbot.reset_chat()
-    else:
-        chatbot.reset_chat()
-    try:
-        resp = chatbot.get_chat_response(prompt, output="text")
-        user_cache = dict()
-        user_cache['timestamp'] = time.time()
-        user_cache['conversation_id'] = resp['conversation_id']
-        user_cache['parent_id'] = resp['parent_id']
-        user_session[session_id] = user_cache
-        return resp['message']
-    except Exception as e:
-        print(e)
-        return f"发生错误: {str(e)}"
-
-
-
+chatbot = Chatbot(api_key=openai_key)
 
 # 注册消息回调
 @wechat.msg_register(ntchat.MT_RECV_TEXT_MSG)
@@ -66,8 +32,8 @@ def on_recv_text_msg(wechat_instance: ntchat.WeChat, message):
     if from_wxid != self_wxid and not room_wxid:
         
         keyword_input = data["msg"]
-        random_session_id = str(random.randint(100, 99999))
-        wechat_str  = get_chat_response(random_session_id, keyword_input)
+
+        wechat_str = chatbot.ask(keyword_input)
         wechat_instance.send_text(
             to_wxid=from_wxid,
             content=wechat_str
@@ -81,9 +47,7 @@ def on_recv_text_msg(wechat_instance: ntchat.WeChat, message):
 
         keyword_input = data['msg']
 
-        random_session_id = str(random.randint(100, 99999))
-
-        wechat_str  = get_chat_response(random_session_id, keyword_input)
+        wechat_str = chatbot.ask(keyword_input)
 
         wechat_instance.send_room_at_msg(
             to_wxid=room_wxid,
